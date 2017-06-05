c3fe679ca7255ea759497682e495b5ef254e8b31
Keisha Leger <Keisha Leger>
2017-06-05 13:55:51 -0400

    added bootstrap and variables

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+*Use Strict*
+
+//array of list of words
+
+var flowers = ["daisy", "lilac", "lotus", "pansy", "peony", "tulip", "viola", "bloom"]
+var randomWord = Math.floor(Math.random() * flowers.length);
+
+//alphabet
+var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+ "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+
+
+$(document).on("click"){
+	
+}
+
+
+
+
+
+
+
+//retrieve user guesses
+
+//correct letters display on screen
+
+//wrong letters result in man on the gallow
