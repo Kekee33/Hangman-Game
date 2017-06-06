f73033914d48f992a43d73ff6bc365cfb9eb0461
Keisha Leger <Keisha Leger>
2017-06-06 16:48:39 -0400

    muted functions to work on one at a time

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,16 @@
 *Use Strict*
 
+//alphabet
+var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
+ "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+function letters() {
+	document.write(alphabet);
+}
+
+letters();
+document.write(alphabet);
+
 //array of list of words
 var randomWord
 var flowers = ["daisy", "lilac", "lotus", "pansy", "peony", "tulip", "viola", "bloom"]
@@ -10,41 +21,38 @@ var counter = 0;
 
 //need function to get list of words
 
-function randomWord() {
+//function randomWord() {
 	for (var i = 0; i < rw; i++)
 }
 
 //empty array to store the answers
-var answerArray = [];
-function start() {
-	for (var i = 0; 1 < rw.length; i++)
-		answerArray[i] = "_";
+//var answerArray = [];
+//function start() {
+	//for (var i = 0; 1 < rw.length; i++)
+		//answerArray[i] = "_";
 }
 
-//alphabet
-var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
- "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 //create event when key is pressed and display on screen
 //retrieve user guesses
 
-function keyRelease(event){
-	var alphabet = document.write(event.key);
+//function keyRelease(event){
+	//var alphabet = document.write(event.key);
 }
 
 //wrong letters result in man on the gallow 
 
-var input = ("guessed letter");
+//var input = ("guessed letter");
 
-for (var i = 0; i != flowers.length; i++){
-		if (flowers.indexOf(flowers[i])
+//for (var i = 0; i != flowers.length; i++){
+		//if (flowers.indexOf(flowers[i])
 
-	if(input==flowers)
-		document.write();
-	else(document.write("try again"));
+	//if(input==flowers)
+		//document.write();
+	//else(document.write("try again"));
 	
 }
 
 //display wins and loss
 
-document.addEventListener("keyUp", keyRelease);
+//document.addEventListener("keyUp", keyRelease);
