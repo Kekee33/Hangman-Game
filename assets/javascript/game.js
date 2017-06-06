9f17029efaaece827ec5782b0e8cf3fe2b385663
Keisha Leger <Keisha Leger>
2017-06-06 10:22:32 -0400

    added non-working functions

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,20 +5,36 @@
 var flowers = ["daisy", "lilac", "lotus", "pansy", "peony", "tulip", "viola", "bloom"]
 var rw = Math.floor(Math.random() * flowers.length);
 
+//need function to get list of words
+function randomword() {
+	var rw = flowers[Math.floor(Math.random() * flowers.length];
+
+}
 
 //alphabet
 var alphabet= ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
  "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-$(document).on("click"){
+//create event when key is pressed and display on screen
+//retrieve user guesses
 
+function keyRelease(event){
+	var alphabet = document.write(event.key);
 }
 
-//retrieve user guesses
+//wrong letters result in man on the gallow 
+
+var input = ("guessed letter");
+
 for (var i = 0; i != flowers.length; i++){
 		if (flowers.indexOf(flowers[i])
+
+	if(input==flowers){
+		document.write();
+	else(document.write("try again"));
+	}
 }
 
-//correct letters display on screen
+//display wins and loss
 
-//wrong letters result in man on the gallow
+document.addEventListener("keyUp", keyRelease);
